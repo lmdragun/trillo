51bfdebb15b66f06e5efe914943c9169b044b5b8
Lindsey <lmdragun@gmail.com>
2015-05-07 11:01:10 -0400

    some changes

---
+++ static/models/trillo.js
@@ -0,0 +1,4 @@
+//take value from id new-card-text to put in new card
+//generate new card
+//place new card in To Do column (id todo-column / ul class card-list)
+//move card from To Do to Done (id completed-column)
---
+++ static/views/trillo.js
@@ -0,0 +1,10 @@
+var CardView = function CardView(){
+
+}
+
+CardView.prototype = {
+
+	onClick: function onClick (event){
+		var id = event.target.getElementById("new-card-button");
+	}
+}
